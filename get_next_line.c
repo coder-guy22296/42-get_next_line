fc7ee5ec8080b14a8544082f26d8a50523995726
Can Yildirim <cyildiri@student.42.us.org>
2016-10-27 18:09:46 -0700

    algorithm mostly done, still needs to handle end of file in buffer

--- get_next_line.c
+++ get_next_line.c
@@ -25,38 +25,38 @@ int	get_next_line(const int fd, char **line)
 	static t_lmap	*buffer_map = 0;
 	t_lmap			*cur_buff;
 	int				line_len;
-	char	*newline_ptr;
+	char			*newline_ptr;
+	char			*write_buff;
 
-
+	write_buff = ft_strnew(BUFF_SIZE);
 	cur_buff = ft_lmapget(buffer_map, &fd);
 	// if there is no buffer for this file descriptor make one and fill buffer
 	if (cur_buff == 0)
 	{
-		cur_buff = ft_lmapnew(&fd, ft_strnew(sizeof(char) * BUFF_SIZE),
+		cur_buff = ft_lmapnew(&fd, ft_strnew(BUFF_SIZE),
 						(sizeof(char) * (BUFF_SIZE + 1)), sizeof(int));
 		
 		read(fd, cur_buff->content, BUFF_SIZE);
 		ft_lmapadd(&buffer_map, cur_buff);
 	}
-	//attempt read from buffer
-	newline_ptr = (char *)ft_strchr(cur_buff->content, '\n');
-	if (newline_ptr)//newline in buffer
+
+	while(!(newline_ptr = (char *)ft_strchr(cur_buff->content, '\n')))
+	{
+		*line = ft_strjoin(*line, cur_buff->content);
+		read(fd, cur_buff->content, BUFF_SIZE);
+	}
+	if (newline_ptr)//newline in buffer (should always be true at this point in the code!!)
 	{
 		line_len =  newline_ptr - (char *)cur_buff->content;
-		ft_strncpy(*line, cur_buff->content, line_len);
+		ft_strncpy(write_buff, cur_buff->content, line_len);
+		*line = ft_strjoin(*line, write_buff);
+		ft_strclr(write_buff);
 		ft_memmove(cur_buff->content, newline_ptr + 1,
 					BUFF_SIZE - (line_len + 1));
 		read(fd, &cur_buff->content[BUFF_SIZE - (line_len + 1)], line_len + 1);
 	}
-	else//no newline in buffer(repeat until not true)
-	{
-		read(fd, cur_buff->content, BUFF_SIZE);
-		
-		//copy full buffer into output
-		//read full buffer
-	}
-	//
-
+	ft_strdel(cur_buff);
+	ft_strdel(write_buff);
 	//copy till the first \n char into line var
 		// shift the remaining amount in the buffer to the front
 		// note the end of the data in the buffer and fill in the rest with a fresh read
