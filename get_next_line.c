a17cbb60d773bdd92866c467a3a7ef4e16ee9f81
Can Yildirim <cyildiri@student.42.us.org>
2016-10-28 15:48:55 -0700

    code restructuring

--- get_next_line.c
+++ get_next_line.c
@@ -20,33 +20,18 @@
 #include "get_next_line.h"
 #include <fcntl.h>
 
-int	get_next_line(const int fd, char **line)
+static void refil_buffer()
 {
-	static t_lmap	*buffer_map = 0;
-	t_lmap			*cur_buff;
-	int				line_len;
-	char			*newline_ptr;
+
+}
+
+static void handle_partial_Buffer(t_lmap *cur_buff, char *newline_ptr, int bytes_read, char **line)
+{
 	char			*write_buff;
-	int				bytes_read;
+	int				line_len;
 
 	write_buff = ft_strnew(BUFF_SIZE);
-	cur_buff = ft_lmapget(buffer_map, &fd);
-	// if there is no buffer for this file descriptor make one and fill buffer
-	if (cur_buff == 0)
-	{
-		cur_buff = ft_lmapnew(&fd, ft_strnew(BUFF_SIZE),
-						(sizeof(char) * (BUFF_SIZE + 1)), sizeof(int));
-		
-		bytes_read = read(fd, cur_buff->content, BUFF_SIZE);
-		ft_lmapadd(&buffer_map, cur_buff);
-	}
-
-	while(!(newline_ptr = (char *)ft_strchr(cur_buff->content, '\n')) && bytes_read == BUFF_SIZE)
-	{
-		*line = ft_strjoin(*line, cur_buff->content);
-		bytes_read = read(fd, cur_buff->content, BUFF_SIZE);
-	}
-	if (newline_ptr)//newline in buffer (should always be true at this point in the code!!)
+	if (newline_ptr)
 	{
 		line_len =  newline_ptr - (char *)cur_buff->content;
 		ft_strncpy(write_buff, cur_buff->content, line_len);
@@ -55,15 +40,77 @@ int	get_next_line(const int fd, char **line)
 		ft_memmove(cur_buff->content, newline_ptr + 1,
 					BUFF_SIZE - (line_len + 1));
 		if (bytes_read < BUFF_SIZE)
-			cur_buff->content[BUFF_SIZE - (line_len + 1)] = '\0'
+			ft_bzero(&cur_buff->content[bytes_read - line_len - 1], BUFF_SIZE - bytes_read + line_len + 1);
 		else
 			bytes_read = read(fd, &cur_buff->content[BUFF_SIZE - (line_len + 1)], line_len + bytes_read);
 	}
+	//	if buffer not full && newline in buffer
 	else if (bytes_read < BUFF_SIZE)
 	{
 		ft_strncpy(write_buff, cur_buff->content, bytes_read);
 		*line = ft_strjoin(*line, write_buff);
 	}
-	ft_strdel(cur_buff);
-	ft_strdel(write_buff);
+}
+
+static void delete_buffer()
+{
+	
+}
+
+static void cleanup()
+{
+
+}
+
+int	get_next_line(const int fd, char **line)
+{
+	static t_lmap	*buffer_map = 0;
+	t_lmap			*cur_buff;
+	char			*newline_ptr;
+	int				bytes_read;
+	//int				line_len;
+	//char			*write_buff;
+
+	//write_buff = ft_strnew(BUFF_SIZE);
+	cur_buff = ft_lmapget(buffer_map, &fd);
+	//	if there is no buffer for this file descriptor make one and fill buffer
+	if (cur_buff == 0)
+	{
+		cur_buff = ft_lmapnew(&fd, ft_strnew(BUFF_SIZE),
+						(sizeof(char) * (BUFF_SIZE + 1)), sizeof(int));
+		bytes_read = read(fd, cur_buff->content, BUFF_SIZE);
+		ft_lmapadd(&buffer_map, cur_buff);
+	}
+	//	while no newline in buffer && buffer is full
+	while(!(newline_ptr = (char *)ft_strchr(cur_buff->content, '\n')) && bytes_read == BUFF_SIZE)
+	{
+		*line = ft_strjoin(*line, cur_buff->content);
+		bytes_read = read(fd, cur_buff->content, BUFF_SIZE);
+	}
+	//null the rest of the buffer if not full
+	ft_bzero(&cur_buff->content[bytes_read], BUFF_SIZE - bytes_read);
+	handle_partial_Buffer(cur_buff, newline_ptr, bytes_read, line);
+
+	//	if newline in buffer (should always be true at this point in the code!!)
+	/*if (newline_ptr)
+	{
+		line_len =  newline_ptr - (char *)cur_buff->content;
+		ft_strncpy(write_buff, cur_buff->content, line_len);
+		*line = ft_strjoin(*line, write_buff);
+		ft_strclr(write_buff);
+		ft_memmove(cur_buff->content, newline_ptr + 1,
+					BUFF_SIZE - (line_len + 1));
+		if (bytes_read < BUFF_SIZE)
+			ft_bzero(&cur_buff->content[bytes_read - line_len - 1], BUFF_SIZE - bytes_read + line_len + 1);
+		else
+			bytes_read = read(fd, &cur_buff->content[BUFF_SIZE - (line_len + 1)], line_len + bytes_read);
+	}
+	//	if buffer not full && newline in buffer
+	else if (bytes_read < BUFF_SIZE)
+	{
+		ft_strncpy(write_buff, cur_buff->content, bytes_read);
+		*line = ft_strjoin(*line, write_buff);
+	}*/
+	//ft_strdel(write_buff);
+	return (1);
 }
