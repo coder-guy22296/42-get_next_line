91fee0c6967fd9ad899460e83e93840ff4a95eba
Can Yildirim <cyildiri@student.42.us.org>
2016-11-02 15:35:01 -0700

    new testcase that broke the algorithm now fixed, test case in commit

--- get_next_line.c
+++ get_next_line.c
@@ -32,53 +32,61 @@ static void handle_partial_Buffer(t_lmap *cur_buff, char *newline_ptr, char **li
 {
 	char			*write_buff;
 	int				line_len;
+	t_buff			*buffer;
 
+	buffer = (t_buff *)cur_buff->content;
 	printf("Function: \t\tBuffer State: (EOF_IN_BUFFER || NEWLINE_IN_BUFFER)\n");
-	printf("Function: BUFF:\n'%s'\n", ((t_buff *)cur_buff->content)->buffer);
+	printf("Function: BUFF:\n'%s'\n", buffer->buffer);
 	write_buff = ft_strnew(BUFF_SIZE);
-	newline_ptr = (char *)ft_strchr(((t_buff *)cur_buff->content)->buffer, '\n');
+	newline_ptr = (char *)ft_strchr(buffer->buffer, '\n');
 	if (newline_ptr)
 	{
 		printf("Function: \t\tBuffer State: NEWLINE_IN_BUFFER");
-		if (((t_buff *)cur_buff->content)->read_len < BUFF_SIZE)
+		if (buffer->buf_util < BUFF_SIZE)
 				printf(" && EOF_IN_BUFFER");
 		printf("\n");
-		line_len =  newline_ptr - (char *)((t_buff *)cur_buff->content)->buffer;
+		line_len =  newline_ptr - (char *)buffer->buffer;
 		printf("Function: line len: %d\n", line_len);
-		ft_strncpy(write_buff, ((t_buff *)cur_buff->content)->buffer, line_len);
+		ft_strncpy(write_buff, buffer->buffer, line_len);
 		printf("Function: COPY\n");
 		*line = ft_strjoin(*line, write_buff);
 		printf("Function: line: %s\n", *line);
 		ft_strclr(write_buff);
 
-		printf("Function: BUFF:\n'%s'\n", ((t_buff *)cur_buff->content)->buffer);
-		ft_memmove(((t_buff *)cur_buff->content)->buffer, newline_ptr + 1,
-					((t_buff *)cur_buff->content)->read_len - (line_len + 1));
+		printf("Function: BUFF:\n'%s'\n", buffer->buffer);
+		ft_memmove(buffer->buffer, newline_ptr + 1,
+					buffer->buf_util - (line_len + 1));
+		buffer->buf_util -= line_len + 1;
 		printf("Function: SHIFT\n");
-		printf("Function: BUFF:\n'%s'\n", ((t_buff *)cur_buff->content)->buffer);
-		if (((t_buff *)cur_buff->content)->read_len < BUFF_SIZE)
-		{
+		printf("Function: read_len:%d\n", buffer->read_len);
+		printf("Function: line_len:%d\n", line_len);
+		printf("Function: buf_util:%d\n", buffer->buf_util);
+		printf("Function: BUFF:\n'%s'\n", buffer->buffer);
+		//if (buffer->read_len < BUFF_SIZE)
+		//{
 			printf("Function: CLEAR EXTRA\n");
-			ft_bzero(&((t_buff *)cur_buff->content)->buffer[((t_buff *)cur_buff->content)->read_len - line_len - 1], BUFF_SIZE - ((t_buff *)cur_buff->content)->read_len + line_len + 1);
-		}
-		else
-		{
+			ft_bzero(&buffer->buffer[buffer->buf_util], BUFF_SIZE - buffer->buf_util);
+			printf("Function: BUFF:\n'%s'\n", buffer->buffer);
+		//}
+		//else
+		//{
 			printf("Function: FILL BUFF\n");
-			((t_buff *)cur_buff->content)->read_len = read(fd, &((t_buff *)cur_buff->content)->buffer[BUFF_SIZE - (line_len + 1)], line_len + 1);
-			printf("Function: bytes Read3: %d\n", ((t_buff *)cur_buff->content)->read_len);
-		}
+			buffer->read_len = read(fd, &buffer->buffer[buffer->buf_util], BUFF_SIZE - buffer->buf_util);
+			buffer->buf_util += buffer->read_len;
+			printf("Function: bytes Read3: %d\n", buffer->read_len);
+		//}
 	}
 	//	if no newline in buffer
-	else/* if (((t_buff *)cur_buff->content)->read_len < BUFF_SIZE)*/
+	else/* if (buffer->read_len < BUFF_SIZE)*/
 	{
 		printf("Function: \t\tBuffer State: EOF_IN_BUFFER\n");
-		printf("Function: bytes Read: %d\n", ((t_buff *)cur_buff->content)->read_len);
-		ft_strncpy(write_buff, ((t_buff *)cur_buff->content)->buffer, ((t_buff *)cur_buff->content)->read_len);
+		printf("Function: bytes Read: %d\n", buffer->read_len);
+		ft_strncpy(write_buff, buffer->buffer, buffer->buf_util);
 		*line = ft_strjoin(*line, write_buff);
-		ft_strclr(((t_buff *)cur_buff->content)->buffer);
+		//ft_strclr(buffer->buffer);
 	}
 	ft_strdel(&write_buff);
-	printf("Function: BUFF:\n'%s'\n", ((t_buff *)cur_buff->content)->buffer);
+	printf("Function: BUFF:\n'%s'\n", buffer->buffer);
 }
 /*
 static void delete_buffer()
@@ -95,10 +103,13 @@ int	get_next_line(const int fd, char **line)
 {
 	static t_lmap	*buffer_map = 0;
 	t_lmap			*cur_buff;
-	char			*newline_ptr;
+	char			*newline_ptr;	
+	t_buff			*buffer;
 
 	newline_ptr = 0;
 	cur_buff = ft_lmapget(buffer_map, &fd);
+	if (cur_buff)
+		printf("Function: BUFF:\n'%s'\n", ((t_buff *)cur_buff->content)->buffer);
 	*line = ft_strnew(0);//needs protection
 	if (cur_buff == 0)//	if there is no buffer for this file descriptor make one and fill buffer
 	{
@@ -106,24 +117,27 @@ int	get_next_line(const int fd, char **line)
 		cur_buff = ft_lmapnew(&fd, ft_newbuffer(BUFF_SIZE + 1, sizeof(char)),
 						(sizeof(char) * (BUFF_SIZE + 1)), sizeof(int));
 		((t_buff *)cur_buff->content)->read_len = read(fd, ((t_buff *)cur_buff->content)->buffer, BUFF_SIZE);
+		((t_buff *)cur_buff->content)->buf_util += ((t_buff *)cur_buff->content)->read_len;
 		printf("Function: bytes Read1: %d\n", ((t_buff *)cur_buff->content)->read_len);
 		printf("Function: FILL BUFF\n");
-		//printf("Function: '%s'\n", ((t_buff *)cur_buff->content)->buffer);
+		//printf("Function: '%s'\n", buffer->buffer);
 		ft_lmapadd(&buffer_map, cur_buff);
 	}
-	while(!(newline_ptr = (char *)ft_strchr(((t_buff *)cur_buff->content)->buffer, '\n')) && ((t_buff *)cur_buff->content)->read_len == BUFF_SIZE)
+	buffer = (t_buff *)cur_buff->content;
+	while(!(newline_ptr = (char *)ft_strchr(buffer->buffer, '\n')) && buffer->read_len == BUFF_SIZE)
 	{//	while no newline in buffer && buffer is full
 		printf("Function: \t\tBuffer State: FULL_BUFFER\n");
-		printf("Function: BUFF:\n'%s'\n", ((t_buff *)cur_buff->content)->buffer);
-		*line = ft_strjoin(*line, ((t_buff *)cur_buff->content)->buffer);
+		printf("Function: BUFF:\n'%s'\n", buffer->buffer);
+		*line = ft_strjoin(*line, buffer->buffer);
 		printf("Function: line: %s\n", *line);
 		printf("Function: top off buffer\n");
-		((t_buff *)cur_buff->content)->read_len = read(fd, ((t_buff *)cur_buff->content)->buffer, BUFF_SIZE);
-		printf("Function: bytes Read2: %d\n", ((t_buff *)cur_buff->content)->read_len);
+		buffer->read_len = read(fd, buffer->buffer, BUFF_SIZE);
+		buffer->buf_util += buffer->read_len;
+		printf("Function: bytes Read2: %d\n", buffer->read_len);
 	}//null the rest of the buffer if not full
 	printf("I MADE IT BRO\n");
-	if (((t_buff *)cur_buff->content)->read_len != 0)
-		ft_bzero(&((t_buff *)cur_buff->content)->buffer[((t_buff *)cur_buff->content)->read_len], BUFF_SIZE - ((t_buff *)cur_buff->content)->read_len);
+	if (buffer->read_len != 0)
+		ft_bzero(&buffer->buffer[buffer->buf_util], BUFF_SIZE - buffer->buf_util);
 	handle_partial_Buffer(cur_buff, newline_ptr, line, fd);
 	return (1);
 }
@@ -135,7 +149,7 @@ int	get_next_line(const int fd, char **line)
 	static t_lmap	*buffer_map = 0;
 	t_lmap			*cur_buff;
 	char			*newline_ptr;
-	int				((t_buff *)cur_buff->content)->read_len;
+	int				buffer->read_len;
 	int				line_len;
 	char			*write_buff;
 
@@ -146,35 +160,35 @@ int	get_next_line(const int fd, char **line)
 	{
 		cur_buff = ft_lmapnew(&fd, ft_strnew(BUFF_SIZE),
 						(sizeof(char) * (BUFF_SIZE + 1)), sizeof(int));
-		((t_buff *)cur_buff->content)->read_len = read(fd, ((t_buff *)cur_buff->content)->buffer, BUFF_SIZE);
+		buffer->read_len = read(fd, buffer->buffer, BUFF_SIZE);
 		
 	}
 	//	while no newline in buffer && buffer is full
-	while(!(newline_ptr = (char *)ft_strchr(((t_buff *)cur_buff->content)->buffer, '\n')) && ((t_buff *)cur_buff->content)->read_len == BUFF_SIZE)
+	while(!(newline_ptr = (char *)ft_strchr(buffer->buffer, '\n')) && buffer->read_len == BUFF_SIZE)
 	{
-		*line = ft_strjoin(*line, ((t_buff *)cur_buff->content)->buffer);
-		((t_buff *)cur_buff->content)->read_len = read(fd, ((t_buff *)cur_buff->content)->buffer, BUFF_SIZE);
+		*line = ft_strjoin(*line, buffer->buffer);
+		buffer->read_len = read(fd, buffer->buffer, BUFF_SIZE);
 	}
 	//null the rest of the buffer if not full
-	ft_bzero(&((t_buff *)cur_buff->content)->buffer[((t_buff *)cur_buff->content)->read_len], BUFF_SIZE - ((t_buff *)cur_buff->content)->read_len);
+	ft_bzero(&buffer->buffer[buffer->read_len], BUFF_SIZE - buffer->read_len);
 	//	if newline in buffer (should always be true at this point in the code!!)
 	if (newline_ptr)
 	{
-		line_len =  newline_ptr - (char *)((t_buff *)cur_buff->content)->buffer;
-		ft_strncpy(write_buff, ((t_buff *)cur_buff->content)->buffer, line_len);
+		line_len =  newline_ptr - (char *)buffer->buffer;
+		ft_strncpy(write_buff, buffer->buffer, line_len);
 		*line = ft_strjoin(*line, write_buff);
 		ft_strclr(write_buff);
-		ft_memmove(((t_buff *)cur_buff->content)->buffer, newline_ptr + 1,
+		ft_memmove(buffer->buffer, newline_ptr + 1,
 					BUFF_SIZE - (line_len + 1));
-		if (((t_buff *)cur_buff->content)->read_len < BUFF_SIZE)
-			ft_bzero(&((t_buff *)cur_buff->content)->buffer[((t_buff *)cur_buff->content)->read_len - line_len - 1], BUFF_SIZE - ((t_buff *)cur_buff->content)->read_len + line_len + 1);
+		if (buffer->read_len < BUFF_SIZE)
+			ft_bzero(&buffer->buffer[buffer->read_len - line_len - 1], BUFF_SIZE - buffer->read_len + line_len + 1);
 		else
-			((t_buff *)cur_buff->content)->read_len = read(fd, &((t_buff *)cur_buff->content)->buffer[BUFF_SIZE - (line_len + 1)], line_len + ((t_buff *)cur_buff->content)->read_len);
+			buffer->read_len = read(fd, &buffer->buffer[BUFF_SIZE - (line_len + 1)], line_len + buffer->read_len);
 	}
 	//	if buffer not full && newline in buffer
-	else if (((t_buff *)cur_buff->content)->read_len < BUFF_SIZE)
+	else if (buffer->read_len < BUFF_SIZE)
 	{
-		ft_strncpy(write_buff, ((t_buff *)cur_buff->content)->buffer, ((t_buff *)cur_buff->content)->read_len);
+		ft_strncpy(write_buff, buffer->buffer, buffer->read_len);
 		*line = ft_strjoin(*line, write_buff);
 	}
 	ft_lmapadd(&buffer_map, cur_buff);
